bf493ff7a52e32d23b32fbf2020e91739ba9a523
Trinity Paulsen <trinity.paulsen@gmail.com>
2022-03-07 21:01:18 -0800

    initial commit

---
+++ src/main/java/com/trinity/User.java
@@ -0,0 +1,24 @@
+package com.trinity;
+
+public class User {
+
+  private int id;
+  String name;
+
+  public void setId (int i) {
+      this.id = i;
+  }
+
+  public void setName (String n) {
+      this.name = n;
+  }
+
+  public int getId() {
+      return this.id;
+  }
+
+  public String getName() {
+      return this.name;
+  }
+
+}
